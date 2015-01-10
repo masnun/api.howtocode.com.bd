469112bfe8437f6e162081f9f38b22fa61c31248
Abu Ashraf Masnun <masnun@gmail.com>
2015-01-11 04:12:59 +0600

    Initial release

---
+++ application/__init__.py
@@ -0,0 +1,27 @@
+import json, urllib2
+from flask import Flask, render_template, redirect
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return redirect("http://howtocode.com.bd")
+
+@app.route('/contrib/<repo>')
+def contrib(repo):
+    try:
+	    url = "https://api.github.com/repos/howtocode-com-bd/%s.howtocode.com.bd/contributors" % repo
+	    data = json.loads(urllib2.urlopen(url).read())
+
+	    for id, author in enumerate(data):
+	        data[id]['badge_login'] = author['login'].replace("-", "--")
+	        data[id]['commits_url'] = "https://github.com/howtocode-com-bd/%s.howtocode.com.bd/commits/master?author=%s" % (repo, author['login'])
+	    return render_template('contrib.html', data=data)
+    except Exception as ex:
+    	return "Unknown error!"
+
+
+if __name__ == '__main__':
+    app.run()
+
+
